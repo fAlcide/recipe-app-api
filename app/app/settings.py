2cb931e985002ecd47846197f2f71ba260eb01e2
fAlcide <alcide.faucheron@etu.uphf.fr>
2023-07-16 14:13:39 +0200

    Added create user and superuser

--- app/app/settings.py
+++ app/app/settings.py
@@ -92,20 +92,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+        'NumericPasswordValidator',
     },
 ]
 
@@ -133,3 +133,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,52 @@
-from django.db import models # noqa
+"""
+Dtabase models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    # extra fields are for any other fields we pass in
+    def create_user(self, email, password=None, **extra_fields):
+        """Create a new user"""
+        if (not email):
+            raise ValueError('Users must have an email address')
+        # creates a new model that we can then modify
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # set_password is a helper function that comes with BaseUserManager
+        user.save(using=self._db)
+        # using=self._db is for supporting multiple databases
+        user.set_password(password)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create a new super user"""
+        # create a new user
+        user = self.create_user(email, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    # is the user active
+    is_active = models.BooleanField(default=True)
+    # is the user a django admin member
+    is_staff = models.BooleanField(default=False)
+
+    # this is the object manager for the user model
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
